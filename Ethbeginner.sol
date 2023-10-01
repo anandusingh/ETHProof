7081cc08c703ce1b232f52478fc29d234f2bede6
Anandu Singh <146640432+anandusingh@users.noreply.github.com>
2023-10-01 20:11:26 +0530

    Create Ethbeginner.sol

---
+++ Ethbeginner.sol
@@ -0,0 +1,39 @@
+/*
+       REQUIREMENTS
+    1. Your contract will have public variables that store the details about your coin (Token Name, Token Abbrv., Total Supply)
+    2. Your contract will have a mapping of addresses to balances (address => uint)
+    3. You will have a mint function that takes two parameters: an address and a value. 
+       The function then increases the total supply by that number and increases the balance 
+       of the “sender” address by that amount
+    4. Your contract will have a burn function, which works the opposite of the mint function, as it will destroy tokens. 
+       It will take an address and value just like the mint functions. It will then deduct the value from the total supply 
+       and from the balance of the “sender”.
+    5. Lastly, your burn function should have conditionals to make sure the balance of "sender" is greater than or equal 
+       to the amount that is supposed to be burned.
+*/
+
+// SPDX-License-Identifier: MIT 
+pragma solidity 0.8.18;
+contract MyToken {  
+string public Token_name = "My Token"; 
+string public Token_Abbrv = "MT"; 
+uint public Totalsupply = 0;
+
+// mapping variable here
+mapping(address => uint) public Adresstobalance;
+
+// Use of mint function
+function mint(address _address, uint _value) public {
+    Totalsupply += _value;
+    Adresstobalance[_address] += _value;
+}
+
+// Use of burn function
+function burn(address _address, uint _value) public {
+    require(Adresstobalance[_address] >= _value, "You have Insufficient balance");
+    if (Adresstobalance[_address] >= _value) {
+        Totalsupply -= _value;
+        Adresstobalance[_address] -= _value;
+    }
+}
+}
